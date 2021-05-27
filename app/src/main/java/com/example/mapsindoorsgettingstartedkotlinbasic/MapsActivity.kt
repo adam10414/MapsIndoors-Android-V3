714b91f4abd3ec9a0e4ee57efce567571d8a6e48
Tim Mikkelsen <timmikkelsen@Tims-MacBook-Pro.local>
2021-05-27 16:07:44 +0200

    initial commit

---
+++ app/src/main/java/com/example/mapsindoorsgettingstartedkotlinbasic/MapsActivity.kt
@@ -0,0 +1,141 @@
+package com.example.mapsindoorsgettingstartedkotlinbasic
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import com.google.android.material.textfield.TextInputEditText
+import com.mapsindoors.mapssdk.MPDirectionsRenderer
+import com.mapsindoors.mapssdk.MPRoutingProvider
+import com.mapsindoors.mapssdk.MapControl
+import com.mapsindoors.mapssdk.Point
+
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var mapView: View
+    private lateinit var mMapControl: MapControl
+    private lateinit var mSearchFragment: SearchFragment
+    private lateinit var mNavigationFragment: NavigationFragment
+    private lateinit var btmnSheetBehavior: BottomSheetBehavior<FrameLayout>
+    private lateinit var mSearchTxtField: TextInputEditText
+    private var currentFragment: Fragment? = null
+    private val mUserLocation: Point = Point(38.897389429704695, -77.03740973527613, 0.0)
+
+    private var mpDirectionsRenderer: MPDirectionsRenderer? = null
+    private var mpRoutingProvider: MPRoutingProvider? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        //TODO: Instantiate MapsIndoors and assign mMapView from MapFragment
+
+        mSearchTxtField = findViewById(R.id.search_edit_txt)
+        //Listener for when the user searches through the keyboard
+        val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        mSearchTxtField.setOnEditorActionListener { textView, i, _ ->
+            if (i == EditorInfo.IME_ACTION_DONE || i == EditorInfo.IME_ACTION_SEARCH) {
+                if (textView.text.isNotEmpty()) {
+                    //TODO: Call the search method when you have created it following the tutorial
+                }
+                //Making sure keyboard is closed.
+                imm.hideSoftInputFromWindow(textView.windowToken, 0)
+
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
+
+        //ClickListener to start a search, when the user clicks the search button
+        var searchBtn = findViewById<ImageButton>(R.id.search_btn)
+        searchBtn.setOnClickListener {
+            if (mSearchTxtField.text?.length != 0) {
+                //There is text inside the search field. So lets do the search.
+                //TODO: Call the search method when you have created it following the tutorial
+            }
+            //Making sure keyboard is closed.
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
+        }
+
+        var bottomSheet = findViewById<FrameLayout>(R.id.standardBottomSheet)
+        btmnSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        btmnSheetBehavior.addBottomSheetCallback(object : BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    if (currentFragment != null) {
+                        if (currentFragment is NavigationFragment) {
+                            //Clears the direction view if the navigation fragment is closed.
+                            mpDirectionsRenderer?.clear()
+                        }
+                        //Clears the map if any searches has been done.
+                        mMapControl?.let { mapControl ->
+                            mapControl.clearMap()
+                        }
+                        //Removes the current fragment from the BottomSheet.
+                        removeFragmentFromBottomSheet(currentFragment!!)
+                    }
+                    mMapControl.setMapPadding(0, 0, 0, 0)
+                } else {
+                    mMapControl.setMapPadding(0, 0, 0, btmnSheetBehavior.peekHeight)
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+        })
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mapView?.let { view ->
+            //TODO: Init MapControl here
+        }
+    }
+
+    //TODO: Implement methods when described in the tutorial.
+
+    //TODO: Implement methods when described in the tutorial.
+
+
+    fun addFragmentToBottomSheet(newFragment: Fragment) {
+        if (currentFragment != null) {
+            supportFragmentManager.beginTransaction().remove(currentFragment!!).commit()
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.standardBottomSheet, newFragment).commit()
+        currentFragment = newFragment
+        //Set the map padding to the height of the bottom sheets peek height. To not obfuscate the google logo.
+        runOnUiThread {
+            mMapControl.setMapPadding(0, 0, 0, btmnSheetBehavior.peekHeight)
+            if (btmnSheetBehavior.state == BottomSheetBehavior.STATE_HIDDEN) {
+                btmnSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
+        }
+    }
+
+    fun removeFragmentFromBottomSheet(fragment: Fragment) {
+        if (currentFragment == fragment) {
+            currentFragment = null
+        }
+        supportFragmentManager.beginTransaction().remove(fragment).commit()
+        runOnUiThread { mMapControl.setMapPadding(0, 0, 0, 0) }
+    }
+}
---
+++ app/src/main/java/com/example/mapsindoorsgettingstartedkotlinbasic/RouteLegFragment.kt
@@ -0,0 +1,37 @@
+package com.example.mapsindoorsgettingstartedkotlinbasic
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
+import com.mapsindoors.mapssdk.RouteLeg
+
+class RouteLegFragment : Fragment() {
+    private var mRouteLeg: RouteLeg? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_route_leg, container, false)
+    }
+
+    override fun onViewCreated(
+        view: View, @Nullable savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Assigning views
+        val stepsTxtView = view.findViewById<TextView>(R.id.steps_text_view)
+        var stepsString = ""
+        //TODO: Create a string to describe the steps.
+        stepsTxtView.text = stepsString
+    }
+
+    companion object {
+        fun newInstance(routeLeg: RouteLeg?): RouteLegFragment {
+            val fragment = RouteLegFragment()
+            fragment.mRouteLeg = routeLeg
+            return fragment
+        }
+    }
+}
